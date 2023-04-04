ceaf532986feadec5a0911dcef99b457893358a4
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-04 02:02:22 +0000

    write_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,31 @@
 Rails.application.routes.draw do
-  namespace :public do
+  # namespaceにすることでURLにadminを含む
+  namespace :admin do
+    root to: 'homes#top'
+    resources :members, only: [:index, :show, :edit, :update]
+    resources :posts,   only: [:index, :show, :edit, :update]
+  end
+  # scopeにすることでURLにpublicを含まない
+  scope module: :public do
+    root to: 'homes#top'
+    resources :posts,   except: [:new]
+    resources :chats,   only: [:create, :show]
+    get 'homes/about'
     get 'relationships/followings'
     get 'relationships/followers'
   end
-  namespace :public do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'chats/show'
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :admin do
-    get 'members/index'
-    get 'members/show'
-    get 'members/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  devise_for :admins
-  devise_for :members
+
+  # 顧客用
+  # URL /members/sign_in ...
+  devise_for :members, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
