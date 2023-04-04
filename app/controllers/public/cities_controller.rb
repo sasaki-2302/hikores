b7ffc303a275f2c482e8e69a4aafae345b9424a8
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-04 01:56:47 +0000

    create_controller

---
+++ app/controllers/public/cities_controller.rb
@@ -0,0 +1,2 @@
+class Public::CitiesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,33 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'relationships/followings'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :public do
+    get 'chats/show'
+  end
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/edit'
+  end
+  namespace :admin do
+    get 'members/index'
+    get 'members/show'
+    get 'members/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :admins
   devise_for :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
