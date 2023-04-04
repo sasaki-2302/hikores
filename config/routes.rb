ac68ed85a9dedc00216570c6374876de75604381
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-04 10:00:19 +0000

    fix_follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'members/index'
-    get 'members/show'
-    get 'members/edit'
-  end
+  # 顧客用
+  # URL /members/sign_in ...
+  devise_for :members, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   # namespaceにすることでURLにadminを含む
   namespace :admin do
     root to: 'homes#top'
@@ -23,18 +32,4 @@ Rails.application.routes.draw do
     end
     get 'homes/about'
   end
-
-  # 顧客用
-  # URL /members/sign_in ...
-  devise_for :members, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
