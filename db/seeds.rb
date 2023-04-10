20a38e305e272d1134e0264d1757bc4f5f183e36
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-10 08:05:34 +0000

    fix_gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,28 +12,28 @@ Admin.create!(
 )
 
 # https://qiita.com/YoshikawaTakeshi/items/f9365175e5c77d67f6dc　参照
-# require 'csv'
+require 'csv'
 
-# # CSV読み込み
-# file_path = 'lib/prefecture.csv'
-# csv_data = CSV.read(file_path)
+# CSV読み込み
+file_path = 'lib/prefecture.csv'
+csv_data = CSV.read(file_path)
 
-# # 都道府県データ抽出
-# prefectures_list = csv_data.map { |row| row[1] }.uniq
+# 都道府県データ抽出
+prefectures_list = csv_data.map { |row| row[1] }.uniq
 
-# # 市区町村データ抽出
-# cities_list = csv_data.map do |row|
-#     next if row[2] == nil
-#     row[1, 2]
-#   end.compact
+# 市区町村データ抽出
+cities_list = csv_data.map do |row|
+    next if row[2] == nil
+    row[1, 2]
+  end.compact
 
-# # 都道府県データ作成
-# prefectures_list.each do |prefecture|
-#   Prefecture.create!(name: prefecture)
-# end
+# 都道府県データ作成
+prefectures_list.each do |prefecture|
+  Prefecture.create!(name: prefecture)
+end
 
-# # 市区町村データ作成
-# cities_list.each do |prefecture, city|
-#   prefecture = Prefecture.find_by(name: prefecture)
-#   prefecture.cities.create(name: city)
-# end
+# 市区町村データ作成
+cities_list.each do |prefecture, city|
+  prefecture = Prefecture.find_by(name: prefecture)
+  prefecture.cities.create(name: city)
+end
