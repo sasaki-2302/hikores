459cfe16dec81e9846b92ee1413accb9c54d4baf
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-11 07:17:44 +0000

    add_top_link

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,9 +1,13 @@
 class SearchsController < ApplicationController
   def search
+    # 検索に使用
     @city = params[:city_id]
     @prefecture = params[:prefecture_id]
     @content = params[:content]
 
+    # 検索結果で使用
     @records = Post.search_for(@city,@prefecture,@content)
+    @Pref_name = Prefecture.find(@prefecture).name
+    @city_name = City.find(@city).name
   end
 end
