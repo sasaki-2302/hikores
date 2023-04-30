b1f0838962b9b6a00209a9734c534337b7626155
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-04-30 07:14:28 +0000

    fix_post_view

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -6,6 +6,7 @@ class SearchsController < ApplicationController
     @content = params[:content]
     # 検索結果で使用
     @records = Post.search_for(@city,@prefecture,@content).page(params[:page]).per(9)
+    @records_count = Post.search_for(@city,@prefecture,@content).count
     # 投稿時に使用
     @post = Post.new
     @posts = @records
