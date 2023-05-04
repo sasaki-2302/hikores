c67cda917a2c702f06120d4f9b8d08ef61a2472a
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-05-04 09:02:32 +0000

    search_sort

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -4,11 +4,25 @@ class SearchsController < ApplicationController
     @city = params[:city_id]
     @prefecture = params[:prefecture_id]
     @content = params[:content]
-    # 検索結果で使用
-    @records = Post.search_for(@city,@prefecture,@content).page(params[:page]).per(9)
+    # 検索結果で使用　並び替えのため分岐
+    if params[:old]
+      @records = search_result.old.page(params[:page]).per(9)
+    elsif params[:favorite_count]
+      # kaminariの通常の書き方は配列には対応していないためエラーが出る　そのため Kaminari.paginate_array(配列) の記述で回避する必要がある
+      @records = Kaminari.paginate_array(search_result.latest.favorite_count).page(params[:page]).per(9)
+    elsif params[:comment_count]
+      @records = Kaminari.paginate_array(search_result.latest.comment_count).page(params[:page]).per(9)
+    else
+      @records = search_result.latest.page(params[:page]).per(9)
+    end
     @records_count = Post.search_for(@city,@prefecture,@content).count
     # 投稿時に使用
     @post = Post.new
     @posts = @records
+  end
+
+  private
+  def search_result
+    Post.search_for(@city,@prefecture,@content)
   end
 end
